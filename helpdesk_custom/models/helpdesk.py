fff4dbe4b1cd1c4920f60e06587292caf6c4ad40
Luis Rangel <luisrangel2202@gmail.com>
2023-11-17 10:21:19 -0700

    [ADD] 'company_select' field to ticket

--- helpdesk_custom/models/helpdesk.py
+++ helpdesk_custom/models/helpdesk.py
@@ -11,7 +11,19 @@ class HelpdeskTicket(models.Model):
         [('hardware', 'Hardware Problem'), ('software', 'Software Problem'),
          ('connection', 'Connection Problem'), ('security', 'Security Problem'),
          ('os', 'Operating System Problem')], string='Complaint Type')
-
+    company_select = fields.Selection([
+                    ('gbnaat', 'GB Soluciones Naat, S. de R.L. de C.V'),
+                    ('concesionaria', 'Concesionaria El Encierro del Norte, S. de R.L. de C.V'),
+                    ('sistemas', 'Sistemas Industrializados en Comedores, S. de R.L. de C.V'),
+                    ('promotora', 'Promotora Misiones del Norte, S. de R.L. de C.V'),
+                    ('dust', 'Dust Tex de Mexico, SA de CV'),
+                    ('promociones', 'Promociones Delta Juarez S.A. de C.V.'),
+                    ('rm', 'RM HealthCare Products S.A. de C.V.'),
+                    ('rexmed', 'PJ Rex MED LLC.'),
+                    ('medicar', 'Medicar Health Inc.'),
+                    ('promisa', 'PROVEEDORA DE MATERIAL INDUSTRIAL'),
+                    ('dmd', 'DMD CUSTOM CRATES & BOXES INC.'),
+                    ], string='Compañía Origen')
     def send_ticket_mail(self):
         if self.user_id:
             print('user id')
@@ -37,6 +49,7 @@ class HelpdeskTicket(models.Model):
 class HelpdeskTeam(models.Model):
     _inherit = "helpdesk.team"
 
+    
     def method_update(self):
         print("Update")
         teams = self.filtered('use_website_helpdesk_form')
